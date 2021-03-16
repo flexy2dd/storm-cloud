5fef97c637204ee0f761e17a0ca5853c894624ca
flexy2dd <richard@pochot.com>
2021-03-16 07:57:27 +0000

    update white factor

--- run_light.py
+++ run_light.py
@@ -13,6 +13,7 @@ import pprint
 import datetime
 from rpi_ws281x import *
 from modules import thunderlight
+from modules import constant
 
 from dateutil.relativedelta import *
 from dateutil.easter import *
@@ -32,10 +33,27 @@ def debug(message):
 #
 if __name__ == "__main__":
 
+#  strip = Adafruit_NeoPixel(
+#      constant.THUNDERLIGHT_LED_COUNT, 
+#      constant.THUNDERLIGHT_LED_PIN, 
+#      constant.THUNDERLIGHT_LED_FREQ_HZ, 
+#      constant.THUNDERLIGHT_LED_DMA, 
+#      constant.THUNDERLIGHT_LED_INVERT, 
+#      constant.THUNDERLIGHT_LED_BRIGHTNESS, 
+#      constant.THUNDERLIGHT_LED_CHANNEL, 
+#      ws.SK6812_STRIP_RGBW
+#  )
+#  strip.begin()
+#  strip.setPixelColor(0, Color(139, 156, 200,100));
+#  strip.show();
+#  time.sleep(1.0)
+#  strip.setPixelColor(0, Color(0, 0, 255,100));
+#  strip.show();
+
   oThunderlight = thunderlight.thunderlight()
  
   oThunderlight.strike()
  
-  time.sleep(2.0)
+  time.sleep(4.0)
   oThunderlight.turnAllOff()
 
